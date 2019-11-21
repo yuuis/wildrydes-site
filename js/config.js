2ab94abe0126465674a1a191f132b97cae624abe
yu ishiguro <yu_ishigur@fringe81.com>
2019-11-21 10:09:47 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_HleZ5YK8E', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3f3f4erlu0b59a34jlqecb1lbh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
